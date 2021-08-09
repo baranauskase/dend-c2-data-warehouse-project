5b05682352ef94f6973e97020a54fec258d9c342
Edgaras Baranauskas <baranauskas.e@gmail.com>
2021-08-09 21:51:34 +0100

    Added ETL for songs and artists

--- sql_queries.py
+++ sql_queries.py
@@ -19,26 +19,26 @@ time_table_drop = "DROP TABLE IF EXISTS dim_time;"
 
 staging_events_table_create= ("""
     CREATE TABLE stg_events (
-        artist VARCHAR(128) NOT NULL,
+        artist VARCHAR(256),
         auth VARCHAR(16) NOT NULL,
-        firstName VARCHAR(32) NOT NULL,
-        gender VARCHAR(1) NOT NULL,
+        firstName VARCHAR(64),
+        gender VARCHAR(1),
         itemInSession INT NOT NULL,
-        lastName VARCHAR(32) NOT NULL,
-        length NUMERIC(10, 5) NOT NULL,
+        lastName VARCHAR(64),
+        length NUMERIC(10, 5),
         level VARCHAR(8) NOT NULL,
-        location VARCHAR(64) NOT NULL,
+        location VARCHAR(512),
         method VARCHAR(4) NOT NULL,
         page VARCHAR(64) NOT NULL,
         registration BIGINT,
-        sessionId BIGINT,
-        song VARCHAR(64) NOT NULL,
+        sessionId BIGINT NOT NULL,
+        song VARCHAR(256),
         status INT NOT NULL,
         ts BIGINT NOT NULL,
-        userAgent VARCHAR(64) NOT NULL,
+        userAgent VARCHAR(512),
         userId BIGINT,
-        PRIMARY KEY(userId, sessionId, itemInSession)
-    );
+        PRIMARY KEY(sessionId, itemInSession)
+    ) diststyle even;
 """)
 
 staging_songs_table_create = ("""
@@ -47,14 +47,14 @@ staging_songs_table_create = ("""
         artist_id VARCHAR(32) NOT NULL,
         artist_latitude NUMERIC(11, 8),
         artist_longitude NUMERIC(11, 8),
-        artist_location VARCHAR(64),
-        artist_name VARCHAR(64) NOT NULL,
+        artist_location VARCHAR(512),
+        artist_name VARCHAR(256) NOT NULL,
         song_id VARCHAR(32) NOT NULL,
-        title VARCHAR(32) NOT NULL,
+        title VARCHAR(256) NOT NULL,
         duration NUMERIC(10, 5) NOT NULL,
-        year INT NOT NULL,
+        year INT  NOT NULL,
         PRIMARY KEY(song_id)
-    );
+    ) diststyle even;
 """)
 
 songplay_table_create = ("""
@@ -65,61 +65,78 @@ songplay_table_create = ("""
         level VARCHAR NOT NULL,
         song_id VARCHAR,
         artist_id VARCHAR,
-        session_id VARCHAR NOT NULL,
-        location VARCHAR NOT NULL,
-        user_agent VARCHAR NOT NULL
-    );
+        session_id VARCHAR,
+        location VARCHAR,
+        user_agent VARCHAR
+    ) diststyle even;
 """)
 
 user_table_create = ("""
     CREATE TABLE dim_user (
         user_id VARCHAR PRIMARY KEY,
-        first_name VARCHAR NOT NULL,
-        last_name VARCHAR NOT NULL,
-        gender VARCHAR NOT NULL,
-        level VARCHAR NOT NULL
-    );
+        first_name VARCHAR,
+        last_name VARCHAR,
+        gender VARCHAR,
+        level VARCHAR
+    ) diststyle even;
 """)
 
 song_table_create = ("""
     CREATE TABLE dim_song (
-        song_id VARCHAR PRIMARY KEY,
-        title VARCHAR NOT NULL,
-        artist_id VARCHAR NOT NULL,
+        song_id VARCHAR(32) PRIMARY KEY,
+        title VARCHAR(256) NOT NULL,
+        artist_id VARCHAR(32) NOT NULL,
         year INT NOT NULL,
-        duration DECIMAL NOT NULL
-    );
+        duration NUMERIC(10, 5) NOT NULL
+    ) diststyle even;
 """)
 
 artist_table_create = ("""
     CREATE TABLE dim_artist (
-        artist_id VARCHAR PRIMARY KEY,
-        name VARCHAR NOT NULL,
-        location VARCHAR,
-        latitude DECIMAL,
-        longitude DECIMAL
-    );
+        artist_id VARCHAR(32) PRIMARY KEY,
+        name VARCHAR(256) NOT NULL,
+        location VARCHAR(512),
+        latitude NUMERIC(11, 8),
+        longitude NUMERIC(11, 8)
+    ) diststyle all;
 """)
 
 time_table_create = ("""
     CREATE TABLE dim_time (
         start_time TIMESTAMP PRIMARY KEY,
-        hour INT NOT NULL,
-        day INT NOT NULL,
-        week INT NOT NULL,
-        month INT NOT NULL,
-        year INT NOT NULL,
-        weekday INT NOT NULL
-    );
+        hour INT,
+        day INT,
+        week INT,
+        month INT,
+        year INT,
+        weekday INT
+    ) diststyle all;
 """)
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+    COPY stg_events
+    FROM {}
+    CREDENTIALS 'aws_iam_role={}' 
+    FORMAT AS JSON {}
+    REGION 'us-west-2';
+""").format(
+    config['S3']['LOG_DATA'],
+    config['IAM_ROLE']['ARN'],
+    config['S3']['LOG_JSONPATH'],
+)
 
 staging_songs_copy = ("""
-""").format()
+    COPY stg_songs 
+    FROM {}
+    CREDENTIALS 'aws_iam_role={}' 
+    FORMAT AS JSON 'auto'
+    REGION 'us-west-2';
+""").format(
+    config['S3']['SONG_DATA'],
+    config['IAM_ROLE']['ARN']
+)
 
 # FINAL TABLES
 
@@ -130,9 +147,49 @@ user_table_insert = ("""
 """)
 
 song_table_insert = ("""
+INSERT INTO dim_song (
+    song_id,
+    title,
+    artist_id,
+    year,
+    duration
+) SELECT 
+  	ss.song_id,
+    ss.title,
+    ss.artist_id,
+    ss.year,
+    ss.duration
+FROM stg_songs ss
+LEFT JOIN dim_song ds ON ds.song_id = ss.song_id
+WHERE ds.song_id IS NULL;
 """)
 
 artist_table_insert = ("""
+INSERT INTO dim_artist (
+    artist_id,
+    name,
+    location,
+    latitude,
+    longitude
+)
+SELECT
+	ranked_stg_songs.artist_id,
+    ranked_stg_songs.artist_name,
+    ranked_stg_songs.artist_location,
+    ranked_stg_songs.artist_latitude,
+    ranked_stg_songs.artist_longitude
+FROM ( 
+  SELECT 
+      artist_id,
+      artist_name,
+      artist_location,
+      artist_latitude,
+      artist_longitude,
+      RANK() OVER(PARTITION BY artist_id ORDER BY year DESC) RANK
+  FROM stg_songs
+) AS ranked_stg_songs
+LEFT JOIN dim_artist da ON da.artist_id = ranked_stg_songs.artist_id
+WHERE ranked_stg_songs.rank = 1 AND da.artist_id IS NULL;
 """)
 
 time_table_insert = ("""
