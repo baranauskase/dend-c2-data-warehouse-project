41c1f58f45af447237aa940f5dec970188bfe8da
Edgaras Baranauskas <baranauskas.e@gmail.com>
2021-08-10 22:08:30 +0100

    Added docstrings

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,44 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drops all warehouse tables.
+
+    Args:
+        cur: database cursor.
+        conn: database connection.
+
+    Returns: None
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Creates all warehouse tables.
+
+    Args:
+        cur: database cursor.
+        conn: database connection.
+
+    Returns: None
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Database setup process entrypoint. First connection to the database is established,
+    then DDL queries are executed.
+
+    Args: None
+
+    Returns: None
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- etl.py
+++ etl.py
@@ -4,18 +4,47 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Loads data from S3 into Refshift staging tables.
+
+    Args:
+        cur: database cursor.
+        conn: database connection.
+
+    Returns: None
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Loads data from staging tables into fact and dimension tables. The data
+    is transformed, i.e. cleaned and deduped for the purpose of creating high
+    quality facts and dimensions.
+
+    Args:
+        cur: database cursor.
+        conn: database connection.
+
+    Returns: None
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    ETL process entrypoint. First connection to the database is established,
+    then data is loaded into staging tables, and finally data is transformed
+    and loaded into fact and dimension tables.
+
+    Args: None
+
+    Returns: None
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
